fcd534b3f7cf80cb7ee767271631901033e6b9b7
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-13 09:06:35 +0330

    step three, showing data

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -31,11 +31,31 @@ const App = () => {
       height: '300px' ,
       background: 'red'
     }
+  },
+  {
+    label: 'five',
+    style: {
+      width: '100%' ,
+      height: '300px' ,
+      background: 'green'
+    }
   }
 ]
 
+const handleClick = () => {
+
+}
+
   return (
-    <div>App</div>
+    <div className='flex flex-col item-center justify-center'>
+    <h1 className='flex items-center justify-center'>Scroll to a certain section</h1>
+    <button onClick={handleClick}>Click To Scroll</button>
+    {
+      data.map((item, index) => (
+        <div key={index} style={item.style}>{item.label}</div>
+      ))
+    }
+    </div>
   )
 }
 
