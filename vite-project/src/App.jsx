51f13275147bf5bfcc306a3bcb391043ae543fea
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-13 08:59:28 +0330

    step two, array
    create an array of objects for data ( its a dummy data!)

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,39 @@
 import React from 'react'
 
 const App = () => {
+
+  const data = [
+    {
+      label: 'one',
+      style: {
+        width: '100%' ,
+        height: '300px' ,
+        background: 'gray'
+      }
+  }, {
+    label: 'two',
+    style: {
+      width: '100%' ,
+      height: '300px' ,
+      background: 'blue'
+    }
+  },{
+    label: 'three',
+    style: {
+      width: '100%' ,
+      height: '300px' ,
+      background: 'black'
+    }
+  }, {
+    label: 'four',
+    style: {
+      width: '100%' ,
+      height: '300px' ,
+      background: 'red'
+    }
+  }
+]
+
   return (
     <div>App</div>
   )
